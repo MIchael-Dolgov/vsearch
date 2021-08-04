90ac2801ac477161978d7e4f5de386447ed97dad
Michael <mishkadolgov@mail.ru>
2021-08-04 18:33:43 +0300

    main

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -1,6 +1,22 @@
 import mysql.connector
 
 
+class DataBaseError(Exception):
+    pass
+
+
+class ConnectionError(Exception):
+    pass
+
+
+class CredentialsError(Exception):
+    pass
+
+
+class SQLError(Exception):
+    pass
+
+
 class UseDataBase:
     """Диспетчер контекста"""
 
@@ -11,9 +27,16 @@ class UseDataBase:
 
     def __enter__(self) -> "Cursor":
         """Производит настройку соединения"""
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mysql.connector.errors.DatabaseError as err:
+            raise DataBaseError(err)
+        except mysql.connector.errors.InterfaceError as err:
+            raise ConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise CredentialsError(err)
 
     
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
@@ -21,3 +44,7 @@ class UseDataBase:
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_value)
+        elif exc_type:
+            raise exc_type(exc_value)
--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,14 +1,27 @@
+from werkzeug.utils import HTMLBuilder
 from cheker import check_logged_in
+from flask import copy_current_request_context
 from flask import Flask, render_template, request, session
-from vsearch import search_for_letters
-from DBcm import UseDataBase
+from vsearch import search_for_letters as sfl
+from DBcm import DataBaseError, UseDataBase, ConnectionError, CredentialsError, SQLError, DataBaseError
+from threading import Thread
 
 #25.07.21 создать индивидуальный viewlog для каждого пользователя
 
 
 app = Flask(__name__)
 
-app.config["dbconfig"] = {}
+app.config["dbconfig"] = {"host": "",
+                          "user": "",
+                          "password": "",
+                          "database": "", }
+
+
+def insert_users_login(account: dict) -> "name":
+    account = session["logged_in"]
+    account = account[0]
+    user = account[0]
+    return user
 
 
 def null_strings(login: any, password :any) -> bool:
@@ -20,21 +33,6 @@ def null_strings(login: any, password :any) -> bool:
             return False
     else:
         return False
-
-
-def log_request(reg: "flask_request", res: str) -> None:
-    "Возвращает подробный отчёт о запросе и результате базе данных в таблицу Log"
-
-    with UseDataBase(app.config["dbconfig"]) as cursor:
-        _SQL  = """insert into log
-                        (phrase, letters, ip, browser_string, results)
-                        values
-                        (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (reg.form["phrase"],
-                              reg.form["letters"],
-                              reg.remote_addr,
-                              reg.user_agent.browser,
-                              res, ))
 
 
 def users_for_DB(login: str, password: str) -> "Status":
@@ -51,13 +49,13 @@ def users_for_DB(login: str, password: str) -> "Status":
 
         #Проверяет правильность пароля к логину
         if resultLog == True:
-            _SQL = """SELECT * FROM accounts WHERE login = '{0}' AND password = '{1}'""".format(login, password)
+            _SQL = """SELECT * FROM accounts WHERE login = '{0}'
+                   AND password = '{1}'""".format(login, password)
             cursor.execute(_SQL)
             user = cursor.fetchall()
-            user = bool(user)
             
-            if user == True:
-                session["logged_in"] = True
+            if user != None:
+                session["logged_in"] = user
                 return "Вы авторизованы!"
 
             else:
@@ -85,16 +83,23 @@ def do_auth():
 def login():
     login = request.form["login"]
     password = request.form["password"]
+    try:
+        if null_strings(login, password) == True:
+            user = users_for_DB(login, password)
+            return render_template("info.html",
+                                the_info = user)
 
-    if null_strings(login, password) == True:
-        user = users_for_DB(login, password)
-        return render_template("info.html",
-                               the_info = user)
-
-    else:
-        user = "Введите логин и пароль"
-        return render_template("user.html",
-                               the_user = user)
+        else:
+            user = "Введите логин и пароль"
+            return render_template("user.html",
+                                   the_user = user)
+    except DataBaseError as err:
+        print("База данных выключена")
+    except Exception as err:
+        print("Непредвиденное исключение: ", str(err))
+    return render_template("info.html",
+                           the_info="Ошибка авторизации",
+                           the_words="В данный момент нельзя авторизоваться",)
 
 
 @app.route("/logout")
@@ -108,12 +113,48 @@ def logout():
 
 @app.route("/search4", methods=["POST"])
 def do_search() -> "html":
-    """asdfasdfdsaf"""
+    """fdasfasdf"""
+
+    @copy_current_request_context
+    def log_request(reg: "flask_request", res: str) -> None:
+        """Возвращает подробный отчёт о запросе и результате базе данных
+         в таблицу Log в режиме многопоточности"""
+
+        user = insert_users_login(session["logged_in"])
+
+        try:
+
+            with UseDataBase(app.config["dbconfig"]) as cursor:
+                _SQL  = """insert into log
+                                (phrase, letters, ip, browser_string, results, users_result)
+                                values
+                                (%s, %s, %s, %s, %s, %s)"""
+                cursor.execute(_SQL, (reg.form["phrase"],
+                                    reg.form["letters"],
+                                    reg.remote_addr,
+                                    reg.user_agent.browser,
+                                    res,
+                                    user))
+        except ConnectionError as err:
+            print("База данных выключена: ", str(err))
+        except DataBaseError as err:
+            print("Ошибка базы данных: ", str(err))
+        except CredentialsError as err:
+            print("Пароль или логин неверный", str(err))
+        except SQLError as err:
+            print("Неверная SQL комманда", str(err), "|", user)
+        except Exception as err:
+            print("Непредвиденное исключение: ", str(err))
+
     phrase = request.form["phrase"]
     letters = request.form["letters"]
     title = "Here are your results: "
-    results = str(search_for_letters(phrase, letters))
-    log_request(request, results)
+    results = str(sfl(phrase, letters))
+    try:
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
+    except Exception as err:
+        print("Возникла ошибка: ", str(err))
     return render_template("results.html",
                            the_phrase=phrase,
                            the_letters=letters,
@@ -125,31 +166,56 @@ def do_search() -> "html":
 @app.route("/entry")
 def entry_page() -> str:
     """fasdfasdfsadf"""
-
-    with  UseDataBase(app.config["dbconfig"]) as cursor:
-        _SQL = """SELECT COUNT(1) FROM log;"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
+    try:
+        with UseDataBase(app.config["dbconfig"]) as cursor:
+            _SQL = """SELECT COUNT(id) FROM log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchone()
+        return render_template("entry.html",
+                               the_title="Search for vowels",
+                               the_words = contents[0])
+    except ConnectionError as err:
+        print("База данных выключена: ", str(err))
+    except DataBaseError as err:
+        print("Ошибка базы данных: ", str(err))
+    except CredentialsError as err:
+        print("Пароль или логин неверный", str(err))
+    except SQLError as err:
+        print("Неверная SQL комманда", str(err))
+    except Exception as err:
+        print("Непредвиденное исключение: ", str(err))
     return render_template("entry.html",
                            the_title="Search for vowels",
-                           the_words = contents)
+                           the_words="""В данный момент количество всех результатов
+                                        поисков пользователей недоступно""")
 
 
 @app.route("/viewlog")
 @check_logged_in
-def view_the_log() -> "html":
+def view_the_log() -> "HTMLBuilder":
     """fdasfasdfasdfasd"""
-    contents = list()
-
-    with UseDataBase(app.config["dbconfig"]) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ("phrase", "letters", "Remote_addr", "User_agent", "Results")
-    return render_template("viewlog.html",
+    try:
+        with UseDataBase(app.config["dbconfig"]) as cursor:
+            _SQL = """SELECT phrase, letters, ip, browser_string,
+            results FROM log WHERE users_result = '{}'""".format(insert_users_login(session["logged_in"]))
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ("phrase", "letters", "Remote_addr", "User_agent", "Results")
+        return render_template("viewlog.html",
+                               the_title="Your view Log",
+                               the_row_titles=titles,
+                               the_data=contents,)
+    except ConnectionError as err:
+        print("База данных выключена: ", str(err))
+    except CredentialsError as err:
+        print("Неверный пароль или логин базы данных")
+    except SQLError as err:
+        print("Неверная SQL комманда: ", str(err))
+    except Exception as err:
+        print("Неизвестная дичь: ", str(err))
+    return render_template("info.html",
                            the_title="View Log",
-                           the_row_titles=titles,
-                           the_data=contents)
+                           the_words="В данный момент таблица запросов недоступна")
 
 
 app.secret_key = ""
